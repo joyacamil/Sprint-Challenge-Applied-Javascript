e2d2b25d39b96b57a03665b2963c602a50d08397
Deshauna Rainer <deshaunarainer@gmail.com>
2019-09-13 21:39:23 -0400

    finished header js but getting header issues...will come back to fix

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,26 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+const headerContainer = document.querySelector('.header-container');
 
-function Header() {}
+function Header(dateObj,h1Obj,tempObj) {
+  let theHeader = document.createElement('div');
+  let theDate = document.createElement('span').textContent = dateObj;
+  let h1 = document.createElement('h1').textContent = h1Obj;
+  let theTemp = document.createElement('span').textContent = tempObj;
+
+  
+  theHeader.classList.add(header);
+  theDate.classList.add(date);
+  theTemp.classList.add(temp);
+
+ 
+
+  headerContainer.prepend('header');
+  header.prepend('date');
+  header.appendChild('h1');
+  header.appendChild('span');
+}
+
+const newHeader = Header('SMARCH 28, 2019', 'Lambda Times', '98°');
+headerContainer.appendChild(newHeader);
