29fca3a315b58fd2ed32b031bfd062985bdb7ccd
Deshauna Rainer <deshaunarainer@gmail.com>
2019-09-13 22:09:11 -0400

    finished tab; not sure if it works but im not getting errors so im hopeful :)

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,32 +8,31 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
-const headerContainer = document.querySelector('.header-container');
+// const headerContainer = document.querySelector('.header-container');
 
-function Header(dateObj,hOneObj,tempObj) {
-  // Pulling Elements
-  let theHeader = document.createElement('div');
-  let theDate = document.createElement('span');
-  let h1 = document.createElement('h1');
-  let theTemp = document.createElement('span');
+// function Header(dateObj,hOneObj,tempObj) {
+//   // Pulling Elements
+//   let theHeader = document.createElement('div');
+//   let theDate = document.createElement('span');
+//   let h1 = document.createElement('h1');
+//   let theTemp = document.createElement('span');
 
-  theDate.textContent = dateObj;
-  h1.textContent = hOneObj;
-  theTemp.textContent = tempObj;
+//   theDate.textContent = dateObj;
+//   h1.textContent = hOneObj;
+//   theTemp.textContent = tempObj;
   
-  // Added Classes
-  theHeader.classList.add('header');
-  theDate.classList.add('date');
-  theTemp.classList.add('temp');
+//   // Added Classes
+//   theHeader.classList.add('header');
+//   theDate.classList.add('date');
+//   theTemp.classList.add('temp');
 
-// Structure
-  headerContainer.prepend(header);
-  header.appendChild(date);
-  header.appendChild(h1);
-  header.appendChild(temp);
+// // Structure
+//   headerContainer.prepend(header);
+//   header.appendChild(date);
+//   header.appendChild(h1);
+//   header.appendChild(temp);
 
-  return header;
-}
+//   return header;
+// }
 
-Header('SMARCH 28, 2019', 'Lambda Times', '98°');
-
+// Header('SMARCH 28, 2019', 'Lambda Times', '98°');
